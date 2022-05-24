9049a51ca0d343dfc5774bce74ec78125f7b97e5
EvgenKh <hamitcevich@gmail.com>
2022-05-24 19:52:00 +0300

    New changes

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// alert(a+b);
+
+// let num = Math.round(Math.random() * 100);
+
+// let abc = ((0.6 * 10) + (0.7 * 10)) / 10;
+// let abc = (0.6 + 0.7).toFixed(1);
+
+let str = '100$';
+let number = parseInt(str) ;
+console.log(number);
