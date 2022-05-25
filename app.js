7a96dde17ebcecbf60d5a2d476e7f379214f2c9a
EvgenKh <hamitcevich@gmail.com>
2022-05-25 17:54:42 +0300

    Homework on strings(Task#2)

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-// 1. Получить число pi из Math и округлить его до 2-х знаков после точки
-let pi = Number((Math.PI).toFixed(2));
-console.log(pi);
+// 1. Получить первую и последнюю буквы строки
+const string = `some test string`;
+let first_letter = string[0];
+let last_letter = string.slice(-1);
+console.log(first_letter, last_letter);
 
-// 2. Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51
-let max = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
-let min = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
-console.log(max, min);
+// 2. Сделать первую и последнюю буквы в верхнем регистре
+let first_and_last_letters_upper =  `${string[0].toLocaleUpperCase()}${string.slice(1,-1)}${string[string.length - 1].toLocaleUpperCase()}` ;
+console.log(first_and_last_letters_upper);
 
-// 3. Работа с Math.random:
-// a. Получить случайное число и округлить его до двух цифр после запятой
-// b. Получить случайное целое число от 0 до X. X - любое произвольное число.
-let random_a = (Math.random()).toFixed(2);
-let random_b = Math.round((Math.random()) * 10);
-console.log(random_a, random_b);
+// 3. Найти положение слова ‘string’ в строке
+let search_str = string.indexOf(`string`);
+console.log(search_str);
 
-// 4. Проверить результат вычисления 0.6 + 0.7 - как привести к нормальному виду (1.3)?
-// let a =  Number((0.6 + 0.7).toFixed(1));
-let b = ((0.6 * 10) + (0.7 * 10)) / 10;
-console.log(b);
+// 4. Найти положение второго пробела (“вручную” ничего не считать)
+let first_space = string.indexOf(` `);
+let second_space = string.indexOf(` `, first_space + 1);
+console.log(second_space);
 
-// 5. Получить число из строки ‘100$’
-let number = parseInt(`100$`);
-console.log(number);
+// 5. Получить строку с 5-го символа длиной 4 буквы
+let str_sub = string.substring(5, 5 + 4);
+console.log(str_sub);
+
+// 6. Получить строку с 5-го по 9-й символы
+let str_substring = string.substring(5, 9);
+console.log(str_substring);
+
+// 7. Получить новую строку из исходной путем удаления последних 6-и символов (то есть исходная строка без последних 6и символов)
+let str_slice = string.slice(0, string.length-6);
+console.log(str_slice);
+
+// 8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет содержаться текст “2016”
+let a = 20, b = 16;
+console.log(String(a) + b);
