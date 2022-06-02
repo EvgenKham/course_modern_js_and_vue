5a54360dfa1026f927c61c4e921ce8e4bc35d1c0
EvgenKh <hamitcevich@gmail.com>
2022-06-02 22:58:12 +0300

    Homework on context call function(this). Tasks#1-6

--- app.js
+++ app.js
@@ -1,77 +1,118 @@
-// 1. Создать две функции:
-// - первая функция принимает массив и колбэк (одна для всех вызовов)
-// - вторая функция (колбэк) обрабатывает каждый элемент массива (для каждого вызова свой callback)
+// 1. Создать объект, который описывает ширину и высоту
+// прямоугольника, а также может посчитать площадь фигуры:
+// const rectangle = {width:..., height:..., getSquare:...};
 
-// Первая функция возвращает строку “New value: ” и результат обработки:
-
-// firstFunc([‘my’, ‘name’, ‘is’, ‘Trinity’], handler1) → “New value: MyNameIsTrinity”
-// firstFunc([10, 20, 30], handler2) → “New value: 100, 200, 300,”
-// firstFunc([{age: 45, name: ‘Jhon’}, {age: 20, name: ‘Aaron’}], handler3) →
-// “New value: Jhon is 45, Aaron is 20,”
-// firstFunc([‘abc’, ‘123’], handler4) → “New value: cba, 321,” // строки инвертируются
-
-
-// Подсказка: secondFunc должна быть представлена функцией, которая принимает
-// один аргумент (каждый элемент массива) и возвращает результат его обработки
-
-
-function firstFunc (arr, fn) {
-
-    let result = 'New value: ';
-
-    for(let i = 0; i < arr.length; i++){
-        result += fn(arr[i]);
+let rectangle = {
+    width: 2, 
+    height: 4,
+    getSquare: function() {
+        return this.width * this.height
     }
-
-    return result;
 }
 
-function secondFunc1 (value) {
-    return value[0].toUpperCase() + value.slice(1);
-}
+console.log(rectangle.getSquare());
 
-console.log(firstFunc(['my', 'name', 'is', 'Trinity'], secondFunc1));
+/*2. Создать объект, у которого будет цена товара и его скидка, а также
+    два метода: для получения цены и для расчета цены с учетом скидки:
+    const price = {
+    price: 10,
+    discount: '15%',
+    ... };
 
-function secondFunc2(value) {
-    return Number(value) * 10 + ", ";
-}
+    price.getPrice(); // 10
+    price.getPriceWithDiscount(); // 8.5
+*/
 
-console.log(firstFunc([10, 20, 30], secondFunc2) )
+const price = {
+    price: 10,
+    discount: '15%',
+    getPrice: function() {return this.price;},
+    getPriceWithDiscount: function() {
+        discountInt = parseInt(this.discount);
+        return this.price - (this.price * discountInt / 100);
+    },
+};
 
-function secondFunc3(value) {
-    return value.name + ' is ' + value.age + ", ";
-}
+console.log(price.getPrice());
+console.log(price.getPriceWithDiscount());
 
-console.log(firstFunc([{age: 45, name: 'Jhon'}, {age: 20, name: 'Aaron'}], secondFunc3));
+/*3. Создать объект, у которого будет поле высота и метод “увеличить
+    высоту на один”. Метод должен возвращать новую высоту:
+    object.height = 10;
+    object.inc(); // придумать свое название для метода
+    object.height; // 11;
+*/
 
-function secondFunc4(value) {
-    return value.split("").reverse().join("") + ', ';
-}
-
-console.log(firstFunc(['abs', '123'], secondFunc4));
-
-
-// 2. Написать аналог метода every. Создайте функцию every, она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив) вторым аргументом callback (обязательно проверьте что передана функция)
-// функция должна возвращать true или false в зависимости от результата вызова callback (проверить число больше 5). Callback  должен принимать один элемент массива, его индекс в массиве и весь массив. 
- 
-let exampleArr = [12, 3, 8, 0, 34, 5, 77];
-
-function every(arr, fn) {
-    if(!Array.isArray(arr)) {
-        return new Error('The first argument expected as array');
+const obj = {
+    height: 3,
+    getHeightPlusOne: function() {
+        return ++this.height ;
     }
-    if (typeof(fn) !== "function") {
-        return new Error('The second argument expected as function');
-    }
-    for(let i = 0; i < arr.length; i++) {
-        if(!(fn(arr[i], i, arr)))
-            return false;
-    }
-
-    return true;
 }
 
+console.log(obj.height);
+console.log(obj.getHeightPlusOne());
+console.log(obj.height);
 
-console.log(every(exampleArr, function(el) {
-    return typeof el === 'number';
-}));
+/* 4. Создать объект “вычислитель”, у которого есть числовое свойство
+    “значение” и методы “удвоить”, “прибавить один”, “отнять один”.
+    Методы можно вызывать через точку, образуя цепочку методов:
+    const numerator = {
+        value: 1,
+        double: function () {...},
+        plusOne: function () {...},
+        minusOne: function () {...},
+    }
+    numerator.double().plusOne().plusOne().minusOne();
+    numerator.value // 3
+*/
+
+const numerator = {
+    value: 3,
+    double: function () { 
+        this.value *= this.value; 
+        return this; 
+    },
+    plusOne: function () { 
+        this.value = ++this.value ; 
+        return this; 
+    },
+    minusOne: function () { 
+        this.value = --this.value; 
+        return this; 
+    },
+}
+
+numerator.double().plusOne().plusOne().minusOne();
+numerator.double().plusOne().minusOne();
+console.log(numerator.value);
+
+
+// 5. Создать объект с розничной ценой и количеством продуктов.
+// Этот объект должен содержать метод для получения общей стоимости
+// всех товаров (цена * количество продуктов)
+
+const product = {
+    price: 17,
+    quantityOfProduct : 5,
+    getPriceQuantities: function () {
+        return this.price * this.quantityOfProduct;
+        
+    }
+};
+
+// 6. Создать объект из предыдущей задачи. 
+// Создать второй объект, который описывает количество деталей и цену за одну деталь. 
+// Для второго объекта нужно узнать общую стоимость всех деталей, но нельзя создавать новые функции и методы.
+// Для этого “позаимствуйте” метод из предыдущего объекта.
+
+product.getPriceQuantities();
+
+const detail = {
+    price: 6,
+    quantityOfDetail: 50,
+};
+
+let getPrice = product.getPriceQuantities.call(detail);
+
+console.log(getPrice);
