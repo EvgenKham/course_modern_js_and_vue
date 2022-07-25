07e1a49bfce57cdeed1594a25a879a92e2676633
EvgenKh <hamitcevich@gmail.com>
2022-07-26 01:34:53 +0300

    Homework on events. Realization dropdown menu.

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
-// 1.По нажатию на кнопку "btn-msg" должен появиться алерт с тем текстом,
-//  который находится в атрибуте data-text у кнопки.
-const btn = document.getElementById("btn-msg");
+// Реализовать примитивный дропдаун. Изначально все dropdown-menu скрыты через класс .d-none. 
+// При клике на dropdown-item должен отображаться блок dropdown-menu, который вложен именно в тот dropdown-item
+// на котором произошел клик. При повторном клике на этот же dropdown-item блок dropdown-menu должен закрыться.
+// При клике на любой другой dropdown-item уже открытый dropdown-menu должен закрываться,
+// а на тот который кликнули открываться.
 
-// btn.addEventListener('click', function(e) {
-//     alert(btn.dataset.text);
+//---Мой пример решения---
+// Вопросы: правильно ли решать через "навешивание" обработчика события на общего предка(<ul class="menu">)?
+// Обязательно ли использовать перебор всех <li class="dropdown-item">, как препод. примере?
+// Почему у меня работает через target, а в препод. примере через currentTarget?
+// const menu = document.querySelector(".menu");
+// let openedMenu = null;
+// let dropdownMenu = null;
+
+// menu.addEventListener('click', e => {
+
+//     let changed = null;
+
+//     if(e.target.className === "dropdown-item"){
+//         dropdownMenu = e.target.querySelector(".dropdown-menu");
+//         changed = dropdownMenu.classList.toggle("d-none");
+//     };
+//     if(openedMenu && openedMenu !== dropdownMenu) {
+//         openedMenu.classList.add("d-none");
+//     };
+//     if(!changed){
+//         openedMenu = dropdownMenu;
+//     };
 // });
 
-// 2. При наведении указателя мыши на "btn-msg", кнопка становится красной; 
-// когда указатель мыши покидает кнопку, она становится прежнего цвета. 
-// Цвет менять можно через добавление класса.
 
-// btn.addEventListener('mouseover', e => {
-//     btn.setAttribute("class", "red");
-// });
-// btn.addEventListener('mouseout', e => {
-//     btn.removeAttribute("class");
-// });
+// !!!!!Два одновременно примера не работают, нужно включать по одному!!!!!
+// ---Пример решения от преподавателя---
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+// В данной переменной мы будем хранить текущее открытое меню
+let currentOpenedMenu = null;
 
-// 3. При нажатии на любой узел документа показать в элементе с id=tag имя тега нажатого элемента.
+function toggleDropdownMenu(e) {
+  // получаем блок меню внутри .dropdown-item
+  const menu = e.currentTarget.querySelector('.dropdown-menu');
+  // метод toggle возвращает булевое значение, если клас удален то вернет false а если добавлен то true. Это значение мы сохраняем в переменную
+  const isAdded = menu.classList.toggle('d-none');
+  // если в currentOpenedMenu уже есть блок и он не равен текущем menu то мы его скрываем
+  if (currentOpenedMenu && currentOpenedMenu !== menu) {
+    currentOpenedMenu.classList.add('d-none');
+  }
+  // Если класс d-none был удален то значет меню видимо и мы его сохраняем в переменную currentOpenedMenu
+  if (!isAdded) {
+    currentOpenedMenu = menu;
+  }
+}
 
-const tag = document.getElementById('tag ');
-document.addEventListener('click', e => {
-    tag.textContent = `Tag: ${e.target.nodeName}`;
-});
-
-// 4. При нажатии на кнопку btn-generate добавлять в список ul элемент списка Li с текстом Item + порядковый номер Li по списку, т.е Item 3, Item 4 и т.д 
-
-const btnGenerate = document.getElementById("btn-generate");
-const ul = document.querySelector("ul");
-btnGenerate.addEventListener('click', e => {
-    const newLi = document.createElement("li");
-    newLi.textContent = `Item ${ul.children.length + 1}`;
-    ul.append(newLi);
-});
+dropdownItems.forEach(d => d.addEventListener('click', toggleDropdownMenu));
