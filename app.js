78050baa20b67fed3ed69aff6d958a11b20ec688
EvgenKh <hamitcevich@gmail.com>
2022-08-01 14:04:50 +0300

    First project(Todo list).original

--- app.js
+++ app.js
@@ -1,42 +1,148 @@
-// 1. При наведении на любой из блоков с классом .box все его дочерние элементы должны поменять свой фон на один из списка. ВАЖНО, только дочерние относительно блока на который навели мышь.
+const tasks = [
+  {
+    _id: '5d2ca9e2e03d40b326596aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095c1288e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+  {
+    _id: '5d2ca9e2e03d40b3232496aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095564788e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+];
 
-// Вот массив (список) рандомных цветов:
-// ['red', 'blue', 'olive', 'orange', 'pink', 'yellow', 'green', 'gray', 'aqua', 'brown'];
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce(( acc, task ) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
 
-// 2. Возращаете фон обратно когда пользователь уводит мышку с блока.
+  //Element UI
+  const listContainer = document.querySelector(".tasks-list-section .list-group");
 
-// 3. Добавление фона из первой части задания сделать с задержкой в 200мс. Т.е каждый последующий блок должен изменить свой фон за 200мс позже предыдущего. Например если первый блок поменял через 200мс то следующий должен поменять через 400 и т.д.
+  const form = document.forms.addTask;
+  const inputTitle = form.elements["title"];
+  const inputBody = form.elements["body"];
 
-const colors = ['red', 'blue', 'olive', 'orange', 'pink', 'yellow', 'green', 'gray', 'aqua', 'brown'];
-const boxes = document.querySelectorAll(".box");
-const defaultColor = "#fff";
+  //Events
+  renderAllTasks(objOfTasks);
+  form.addEventListener("submit", onFormSubmitHandler);
+  listContainer.addEventListener("click", onDeleteHandler);
 
-//Случайный выбор фона и назначение его элементу
-function choiceRandomColor(e) {
-  const colorIndex = Math.floor( Math.random() * colors.length );
-  e.style.background = colors[colorIndex];
-}
-//Установка фона в прежнее состояние
-function changeDefaultColor(e) {
-  const currentBox = e.target;
-  currentBox.style.background = defaultColor;
-}
+  function renderAllTasks (tasksList) {
+    if(!tasksList) {
+      console.error("Please, enter the list of tasks!");
+      return;
+    }
 
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach( task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    })
 
-function changeColor(e) {
+    listContainer.appendChild(fragment);
+  }
 
-  const currentBox = e.target;
-  //создаем массив и добавляем в него элементы потомков, в которых нужно изменить цвета
-  let elements = [currentBox];
-  let nextChild = currentBox.firstElementChild;
-  //Проверяем есть ли потомки у элемента и добавляем каждый в конец массива
-  while(nextChild) {
-    elements.push(nextChild);
-    nextChild = nextChild.firstElementChild;
-  } 
-  //Проходим по масиву потомков и изменяем цвет через заданный интервал времени
-  elements.forEach((el, index) => setTimeout(choiceRandomColor, 200 * (index + 1), el));
-}
+  function listItemTemplate({_id, title, body} = {}) {
+    const li = document.createElement("li");
+    li.classList.add(
+        "list-group-item", 
+        "d-flex", 
+        "align-items-center", 
+        "flex-wrap", 
+        "mt-2"
+      );
+    li.setAttribute('data-task-id', _id);
+    const span = document.createElement("span");
+    span.textContent = title;
+    span.style.fontWeight = "bold";
 
-boxes.forEach( elem => elem.addEventListener('mouseenter', changeColor));
-boxes.forEach( elem => elem.addEventListener('mouseleave', changeDefaultColor));
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Detete task";
+    deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+
+    const article = document.createElement("p");
+    article.textContent = body;
+    article.classList.add("mt-2", "w-100");
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if(!titleValue || !bodyValue) {
+      alert("Please, enter title and body");
+      return;
+    }
+
+    const task = createNewTask(titleValue, bodyValue);
+    const listItem = listItemTemplate(task);
+    listContainer.prepend(listItem);
+    form.reset();
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    }
+
+    objOfTasks[newTask._id] = newTask;
+
+    return { ...newTask };
+  }
+
+  function deleteTask(id) {
+    const { title } = objOfTasks[id];
+    const isConfirm = confirm(`Are you sure you want to delete the task: ${title} ?`);
+    if(!isConfirm) return isConfirm;
+    delete objOfTasks[id];
+    return isConfirm;
+  }
+
+  function deleteTaskFromHtml(confirmed, el) {
+    if(!confirmed) return;
+    el.remove();
+  }
+
+  function onDeleteHandler({target}) {
+    if(target.classList.contains("delete-btn")){
+      const parent = target.closest("[data-task-id]");
+      const id = parent.dataset.taskId;
+      const confirm = deleteTask(id);
+      deleteTaskFromHtml(confirm, parent);
+    }
+    
+  }
+
+})(tasks);
