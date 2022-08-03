58f744595b648c599bb8a047d4417e5c6afa386b
EvgenKh <hamitcevich@gmail.com>
2022-08-03 21:48:51 +0300

    Homework on first project "ToDo_List". Tasks#1-3.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// 1. Если массив с задачами пустой то под формой нужно выводить сообщение об этом, также это же сообщение нужно выводить если вы удалите все задачи.
+
+// 2. В каждый элемент li добавить кнопку которая будет делать задачу выполненной. завершенные задачи должны быть подсвечены любым цветом.
+
+// 3. Добавить функционал отображения незавершенных задач и всех задач. т.е у вас будет две кнопки над таблицей 1-я "показать все задачи" и 2-я "показать незавершенные задачи", определить завершена задача или нет можно по полю completed в объекте задачи.  По умолчанию при загрузке отображаются все задачи. 
+
+// *Задача со звездочкой. При завершении задачи в разделе "незавершенные задачи" она должна от туда пропадать и быть видна в разделе "все задачи" при этом во всех задачах завершенные задачи могут быть восстановлены. Также в разделе "все задачи" завершенные задачи должны быть в самом низу после открытых задач
+
 const tasks = [
   {
     _id: '5d2ca9e2e03d40b326596aa7',
@@ -30,7 +38,7 @@ const tasks = [
       'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
   },
 ];
-
+//Самовызывающаяся функция-исполнитель
 (function(arrOfTasks) {
   const objOfTasks = arrOfTasks.reduce(( acc, task ) => {
     acc[task._id] = task;
@@ -39,7 +47,7 @@ const tasks = [
 
   //Element UI
   const listContainer = document.querySelector(".tasks-list-section .list-group");
-
+  
   const form = document.forms.addTask;
   const inputTitle = form.elements["title"];
   const inputBody = form.elements["body"];
@@ -48,8 +56,16 @@ const tasks = [
   renderAllTasks(objOfTasks);
   form.addEventListener("submit", onFormSubmitHandler);
   listContainer.addEventListener("click", onDeleteHandler);
+  listContainer.addEventListener("click", onCompleteHanler);
+  const showTaskContainer = document.querySelector(".data-set-show-tasks");
+  showTaskContainer.addEventListener("click", onShowAllTasks);
+  showTaskContainer.addEventListener("click", onShowNoCompletedTasks);
 
+
+  //Отрисовка задач по умолчанию
   function renderAllTasks (tasksList) {
+    //Если список задач пуст - выводим сообщение
+    messageAboutNoTask(tasksList);
     if(!tasksList) {
       console.error("Please, enter the list of tasks!");
       return;
@@ -60,11 +76,13 @@ const tasks = [
       const li = listItemTemplate(task);
       fragment.appendChild(li);
     })
-
+    //Добавляем перед всеми задачами 2 кнопки: "Все" и "Выполненные" задачи
+    listContainer.before(buttonGroupTemplate());
+    //Затем добавляем все задачи
     listContainer.appendChild(fragment);
   }
-
-  function listItemTemplate({_id, title, body} = {}) {
+  //Отрисовка одной задачи в списке
+  function listItemTemplate({_id, title, body, completed} = {}) {
     const li = document.createElement("li");
     li.classList.add(
         "list-group-item", 
@@ -74,13 +92,27 @@ const tasks = [
         "mt-2"
       );
     li.setAttribute('data-task-id', _id);
+    //Выполненые задачи при певоначальной загрузке страницы отображаются светло-зеленым цветом
+    if(completed){
+      li.style.background = "lightgreen";
+    }
+    //По умолчанию все задачи отобразаются
+    li.style.visibility = "visible";
+    
+    //Какого хуя visibility работает, а display - ни хуя!!!!!!!!!!!!!!!!!
+    // li.style.display = "";
+
     const span = document.createElement("span");
     span.textContent = title;
     span.style.fontWeight = "bold";
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Detete task";
-    deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+    deleteBtn.classList.add("btn", "btn-danger", "ml-auto","mb-2" , "delete-btn");
+
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "Complete task";
+    completeBtn.classList.add("btn", "btn-primary", "ml-auto", "mb-2", "complete-btn");
 
     const article = document.createElement("p");
     article.textContent = body;
@@ -88,13 +120,21 @@ const tasks = [
 
     li.appendChild(span);
     li.appendChild(deleteBtn);
+    li.appendChild(completeBtn);
     li.appendChild(article);
 
     return li;
   }
-
+  //Функция-обработчик для добавления пользовательских задач
   function onFormSubmitHandler(e) {
     e.preventDefault();
+
+    //При дабавлении хотя бы одной задачи - уведомление удаляем
+    const article = document.querySelector(".article-message");
+    if(article){
+      article.remove();
+    }
+
     const titleValue = inputTitle.value;
     const bodyValue = inputBody.value;
 
@@ -121,7 +161,7 @@ const tasks = [
 
     return { ...newTask };
   }
-
+  //Показ модального окна с подтвеждением на удаление задачи
   function deleteTask(id) {
     const { title } = objOfTasks[id];
     const isConfirm = confirm(`Are you sure you want to delete the task: ${title} ?`);
@@ -129,20 +169,98 @@ const tasks = [
     delete objOfTasks[id];
     return isConfirm;
   }
-
+  //Удаление задачи
   function deleteTaskFromHtml(confirmed, el) {
     if(!confirmed) return;
     el.remove();
   }
-
+  //Функция-обработчик события по поиску и удалении выбранной задачи
   function onDeleteHandler({target}) {
     if(target.classList.contains("delete-btn")){
       const parent = target.closest("[data-task-id]");
       const id = parent.dataset.taskId;
       const confirm = deleteTask(id);
       deleteTaskFromHtml(confirm, parent);
+      //Проверяем осталась ли хоть одна задача, если нет - выводим сообщение
+      messageAboutNoTask(objOfTasks);
+
     }
-    
+  }
+  //Создание сообщения при отсутсвии задач
+  function messageAboutNoTask(tasks) {
+    if(Object.values(tasks).length === 0) {
+      const article = document.createElement("p");
+      article.textContent = "You don't have tasks!";
+      article.classList.add("mt-2", "text-center", "font-weight-bold", "article-message");
+      listContainer.append(article);
+    }
+  }
+  //Функция-обработчик события для пометки выполненных задач светло-зеленым цветом
+  function onCompleteHanler({target}){
+    if(target.classList.contains("complete-btn")) {
+      const parent = target.closest("[data-task-id]");
+      parent.style.background = "lightgreen";
+      const id = parent.dataset.taskId;
+      objOfTasks[id].completed = true ;
+    }
+  }
+  //Отрисовка группы кнопок для всех и выполненых задач
+  function buttonGroupTemplate () {
+    const div = document.createElement("div");
+    div.classList.add("btn-group", "data-set-show-tasks");
+
+    const buttonAllTasks = document.createElement("button");
+    buttonAllTasks.classList.add("btn", "btn-outline-info", "data-set-all-tasks");
+    buttonAllTasks.textContent = "All Tasks";
+
+    const buttonCompletedTasks = document.createElement("button");
+    buttonCompletedTasks.classList.add("btn", "btn-outline-info", "data-set-no-completed-tasks");
+    buttonCompletedTasks.textContent = "No Completed";
+
+    div.appendChild(buttonAllTasks);
+    div.appendChild(buttonCompletedTasks);
+
+    return div;
+  }
+
+  function hideCompletedTask (idTask) {
+    [...listContainer.children].forEach( li => {
+      if(li.dataset.taskId === idTask){
+        li.style.visibility = "hidden";
+        //Какого хуя visibility работает, а display - ни хуя!!!!!!!!!!!!!!!!!
+        // li.style.display = "none";
+      } 
+    });
+  }
+
+  function showTask (idTask) {
+    [...listContainer.children].forEach( li => {
+      if(li.dataset.taskId === idTask){
+        li.style.visibility = "visible";
+        //Какого хуя visibility работает, а display - ни хуя!!!!!!!!!!!!!!!
+        // li.style.display = "";
+      } 
+    });
+  }
+
+  function onShowAllTasks ({target}) {
+    const btnAll = target.classList.contains("data-set-all-tasks");
+    if(btnAll){
+      Object.values(objOfTasks).forEach(task => {
+          showTask(task._id);
+      }); 
+    }
+  }
+
+  function onShowNoCompletedTasks ({target}) {
+    const btnCompleted = target.classList.contains("data-set-no-completed-tasks");
+    if(btnCompleted){
+      Object.values(objOfTasks).forEach(task => {
+        if(task.completed === true) {
+          hideCompletedTask(task._id);         
+        }   
+      }); 
+    }
   }
 
 })(tasks);
