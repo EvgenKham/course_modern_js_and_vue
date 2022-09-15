3cfdc0032d90e8486bb6ebb97536a29628b31b96
EvgenKh <hamitcevich@gmail.com>
2022-09-15 22:23:40 +0300

    Homework on AJAX. Get_response

---
+++ app.js
@@ -0,0 +1,85 @@
+const btn = document.querySelector("button");
+const container = document.querySelector(".container");
+
+btn.addEventListener("click", renderAboutUser);
+container.addEventListener("click", displayAboutUser);
+
+function getPosts(cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+    xhr.onload = function() {
+        const response = JSON.parse(xhr.responseText);
+        cb(response);
+    }
+
+    xhr.onerror = function() {
+        console.log("error");
+    };
+   
+    xhr.send();
+}
+
+function renderAboutUser() {
+    getPosts((response) => {
+        const fragment = document.createDocumentFragment();
+        response.forEach(user => {
+            const person = document.createElement("div");
+            person.classList.add("user");
+
+            const name = document.createElement("p");
+            name.classList.add("name");
+            name.textContent = `name: ${user.name}`;
+
+            const aboutUser = document.createElement("div");
+            aboutUser.classList.add("about-user", "d-none");
+
+            const userName = document.createElement("p");
+            userName.textContent = `username: ${user.username}`;
+
+            const email = document.createElement("p");
+            email.textContent = `email: ${user.email}`;
+
+            const address = document.createElement("i");
+            address.textContent = "address: ";
+
+            const street = document.createElement("pre");
+            street.textContent = `    street: ${user.address.street}`;
+
+            const suite = document.createElement("pre");
+            suite.textContent = `    suite: ${user.address.suite}`;
+
+            const city = document.createElement("pre");
+            city.textContent = `    city: ${user.address.city}`;
+
+            const phone = document.createElement("p");
+            phone.textContent = `phone: ${user.phone}`
+
+            const website = document.createElement("p");
+            website.textContent = `website: ${user.website}`;
+            
+            aboutUser.appendChild(userName);
+            aboutUser.appendChild(email);
+            aboutUser.appendChild(address);
+            aboutUser.appendChild(street);
+            aboutUser.appendChild(suite);
+            aboutUser.appendChild(city);
+            aboutUser.appendChild(phone);
+            aboutUser.appendChild(website);
+            
+            person.appendChild(name);
+            person.appendChild(aboutUser);
+            fragment.appendChild(person);
+        });
+
+        container.appendChild(fragment);
+    });
+}
+
+function displayAboutUser (e) {
+    if(e.target.className === "name") {
+        changedUser = e.target;
+        const aboutUser = changedUser.nextElementSibling;
+        aboutUser.classList.toggle("d-none");
+   }
+}
