4975e1e8a4978cbe56520f11700336ddf3386204
EvgenKh <hamitcevich@gmail.com>
2022-09-29 15:17:17 +0300

    Kakogo huy ne rabotaet? Gospody pomogy typomy

--- app.js
+++ app.js
@@ -1,91 +1,47 @@
-// Примеры создания классов, обычное наследование и наследование через prototype (ES5)
-// Создание класса Product с его свойствами и методом
-function Product(brand, price, discount) {
-  this.brand = brand;
-  this.price = price;
-  this.discount = discount;
-  this.getPriceWithDiscount = function(){
-    return (this.price * (100 - this.discount)) / 100;
-  };
-}
+// Есть класс Planet
+// function Planet(name) {
+//   this.name = name;
+//   this.getName = function () {
+//       return 'Planet name is ' + this.name;
+//   }
+// }
+// Создать наследника от Planet, который будет называться PlanetWithSatellite и будет
+// принимать, кроме name, название спутника (satelliteName). Переопределите метод
+// getName для PlanetWithSatellite так, чтобы он возвращал ту же самую строчку +
+// дополнительный текст 'The satellite is' + satelliteName.
+// Например:
+// var earth = new PlanetWithSatellite('earth', 'moon');
+// earth.getName(); 
+// 'Planet name is earth. The satellite is moon’
 
-//Добавление нового метода для класса Product через прототип для класса Product
-// НЕЛЬЗЯ использовать стрелочные ф-ции, так как они не имеют собственных this и super
-Product.prototype.setNewPrice = function (newPrice) {this.price = newPrice;} 
+function Planet(name) {
+  this.name = name;
 
-const apple = new Product('Apple', 200, 18);
-const samsung = new Product('Samsung', 150, 13);
+  // Почему при определении метода здесь, он не работает в родительском классе?
+  // TypeError: Cannot read properties of undefined (reading 'call') at PlanetWithSatellite.getName
 
-console.log(apple);
-console.log(apple.getPriceWithDiscount());
-console.log(samsung);
-console.log(samsung.getPriceWithDiscount());
-console.log(samsung instanceof Product);
-apple.setNewPrice(500);
-console.log(apple.getPriceWithDiscount());
-
-
-function Person (firstname, lastname) {
-  this.firstname = firstname;
-  this.lastname = lastname;
-}
-
-//Добавление навых методов в класс через prototype
-Person.prototype.getFullname = function() {
-  return `${this.firstname} ${this.lastname}`;
-};
-Person.prototype.sayHello = function() {
-  return `Hello ${this.firstname} ${this.lastname}`;
-}
-
-
-// Пример наследования в ES5 ( через call() или apply() )
-function User (firstname, lastname, username) {
-  Person.call(this, firstname, lastname);
-  this.username = username;
-}
-// При таком наследовании необходимо переопределять prototype
-User.prototype = Object.create(Person.prototype);
-User.prototype.constructor = Person;
-
-const ivan = new Person('Ivan', 'Nefedov');
-console.log(ivan);
-const user1 = new User('Evgen', 'Khamitcevich', 'EvgenKh');
-console.log(user1);
-
-
-
-// Примеры создания классов и принцыпы написания кода в ES6 (Class/inheritance)
-class Animal{
-  constructor(name) {
-    this.speed = 0;
-    this.name = name;
+  // this.getName = function () {
+  //     return 'Planet name is ' + this.name;
+  //   }
+  
   }
-  run(speed) {
-    this.speed = speed;
-    return (`${this.name} бежит со скоростью ${this.speed}.`);
+
+  // А при определении метода здесь всё работает
+  Planet.prototype.getName = function () {
+    return 'Planet name is ' + this.name;
   }
-  stop(){
-    this.speed = 0;
-    return (`${this.name} стоит неподвижно.`);
+
+function PlanetWithSatellite(name, satelliteName) {
+  Planet.call(this, name)
+  this.satelliteName = satelliteName
+  this.getName = function () {
+    let str = Planet.prototype.getName.call(this);
+    return `${str}. The satellite is ${this.satelliteName}`;
   }
 }
 
-class Rabbit extends Animal{
-  constructor(name, earLength){
-    super(name);
-    this.earLength = earLength;
-  }
-  hide(){
-    return (`${this.name} прячется!`);
-  }
-  stop(){
-    super.stop();
-    this.hide();
-  }
-}
+PlanetWithSatellite.prototype = Object.create(Planet.prototype);
+PlanetWithSatellite.prototype.constructor = Planet;
 
-let rabbit = new Rabbit("Белый кролик", 10);
-
-console.log(rabbit.run(5));
-console.log(rabbit.stop());
+let earth = new PlanetWithSatellite('Earth', 'Moon');
+console.log(earth.getName());
